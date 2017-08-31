303f67524821fef12c731c5aa4c5bdc679323e7d
mashmn <borne.god@gmail.com>
2017-08-31 14:30:56 -0400

    Added Routes to import and moved all routing path to Routes array.

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -2,14 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { StarComponent } from './shared/star.component';
 import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { ProductDetailComponent } from './products/product-detail.component';
-import { WelcomeComponent } from "./home/welcome.component";
+import { WelcomeComponent } from './home/welcome.component';
+
+const appRoutes: Routes = [
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/:id', component: ProductDetailComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
@@ -21,16 +29,12 @@ import { WelcomeComponent } from "./home/welcome.component";
     WelcomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes
+    ),
     BrowserModule,
     FormsModule,
-    HttpClientModule,
-    RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', component: ProductDetailComponent },
-      { path: 'welcome', component: WelcomeComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ])
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
